02eefdd467ade90234d67e9ab8847c7641244c9f
Kaynan Wallen <kaynanwallen@gmail.com>
2024-01-25 19:30:26 -0300

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import './App.css'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Main from './components/main/Main'
 
+
+import ReactGA4 from 'react-ga4';
+
+
 function App() {
+  useEffect(() => {
+    // Inicialize o Google Analytics 4
+    ReactGA4.initialize('G-TNEF00KY3F');
+
+    // Registre a página inicial
+    ReactGA4.send({ hitType: 'pageview', page_path: window.location.pathname });
+  }, []);
+
 
   return (
     <>
